6cc8ffcf3197b0023bf2305d23e0a33a667fef7a
Uma Victor <umavictor11@gmail.com>
2025-06-22 22:30:23 +0100

    feat:add menu to view other pages

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="My Hobbies", url=os.getenv("URL"))
