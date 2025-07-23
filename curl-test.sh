97c5cec1a8b9c549539844f53018655321eb2892
uma-victor1 <umavictor11@gmail.com>
2025-07-23 03:28:54 +0000

    add docker config

--- curl-test.sh
+++ curl-test.sh
@@ -13,10 +13,11 @@ echo "Failed to post timeline to db. Retry"
 exit 1
 fi
 
-reqres=$(curl -f -s -o -X GET http://127.0.0.1:5000/api/timeline_pos)
+reqres=$(curl -f -s -o -X GET http://127.0.0.1:5000/api/timeline_post)
 
 if [[ "$?" -eq 0 ]]; then
 echo "Successfully retrieved timeline"
+echo "$reqres"
 else
 echo "Failed to retreive timeline"
 exit 1
